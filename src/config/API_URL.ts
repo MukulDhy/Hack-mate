1bfbc92fff92150319954a17f16268126b64fcd4
MukulDhy <1029mukul38@gmail.com>
2025-09-04 22:36:04 +0530

    noara more changes ...

--- src/config/API_URL.ts
+++ src/config/API_URL.ts
@@ -1,2 +1,2 @@
-export const API_URL = "http://192.168.0.9:5001"
-export const WS_API_URL = "192.168.0.9:5001"
+export const API_URL = "http://192.168.0.4:5001"
+export const WS_API_URL = "192.168.0.4:5001"
