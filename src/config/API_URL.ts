4d497e711fce4634b613ae865049bd531c93fddb
MukulDhy <1029mukul38@gmail.com>
2025-09-19 19:37:13 +0530

    server wio9ll work now

--- src/config/API_URL.ts
+++ src/config/API_URL.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://hackforce.onrender.com:5001"
-export const WS_API_URL = "wss://hackforce.onrender.com:5001"
+export const API_URL = "https://hackforce.onrender.com"
+export const WS_API_URL = "wss://hackforce.onrender.com"
